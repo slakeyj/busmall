03cde5820dea2bebdab9845b773e50e860e660ec
Susanna Lakey <susannalakey@gmail.com>
2019-07-22 16:14:54 -0700

    random number generator bug fixed

--- js/app.js
+++ js/app.js
@@ -23,29 +23,44 @@ for (var i = 0; i < imageNameList.length; i++) {
   new NewImage(imageNameList[i]);
 }
 
-// helper functions
 
 var recentRandomNumbers = [];
 function getRandomIndex() {
   var randomIndex = getRandomNumber(0, allImages.length - 1);
-
+  console.log('my random index is', randomIndex);
   while (recentRandomNumbers.includes(randomIndex)) {
     randomIndex = getRandomNumber(0, allImages.length - 1);
   }
-  if (recentRandomNumbers.length > 5)
+  console.log('after the while loop random index is', randomIndex);
+  if (recentRandomNumbers.length > 5) {
+    recentRandomNumbers.shift(); // may need to adjust number on this
+  }
+  recentRandomNumbers.push(randomIndex);
+  console.log(randomIndex);
+  console.log(randomIndex);
+  return randomIndex;
 }
 
 
-function getRandomNumber() {
-  return Math.floor(Math.random) * (max - min + 1) + min;
+// helper functions
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function render() {
-  imageOneEl.src = allImages[0].filepath;
-  imageOneEl.alt = allImages[0].name;
-  imageOneEl.title = allImages[0].title;
-  console.log('names', allImages[0].name);
+  var pictureIndex = getRandomIndex();
+  imageOneEl.src = allImages[pictureIndex].filepath;
+  imageOneEl.alt = allImages[pictureIndex].name;
+  imageOneEl.title = allImages[pictureIndex].name;
 
+  pictureIndex = getRandomIndex();
+  imageTwoEl.src = allImages[pictureIndex].filepath;
+  imageTwoEl.alt = allImages[pictureIndex].name;
+  imageTwoEl.title = allImages[pictureIndex].name;
+  pictureIndex = getRandomIndex();
+  imageThreeEl.src = allImages[pictureIndex].filepath;
+  imageThreeEl.alt = allImages[pictureIndex].name;
+  imageThreeEl.title = allImages[pictureIndex].name;
 }
 
 render();
