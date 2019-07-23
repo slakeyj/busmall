a14a5da71b50edd20b9e615c13ec5cf6c381b093
Susanna Lakey <susannalakey@gmail.com>
2019-07-22 20:56:03 -0700

    added some css styling

--- js/app.js
+++ js/app.js
@@ -4,15 +4,19 @@
 var imageOneEl = document.getElementById('image-one');
 var imageTwoEl = document.getElementById('image-two');
 var imageThreeEl = document.getElementById('image-three');
+
 var imageBoxEl = document.getElementById('image-box');
+
 var imageNameList = ['banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg', 'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg', 'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'usb.gif', 'water-can.jpg', 'wine-glass.jpg'];
+
+var ulEl = document.getElementById('vote-list');
 
 var allImages = [];
 var clickCount = 0;
 
-function NewImage(name) {
-  this.name = name.split('.')[0];
-  this.filepath = `img/${name}`;
+function NewImage(filename) {
+  this.name = filename.split('.')[0];
+  this.filepath = `img/${filename}`;
   this.viewCount = 0;
   this.voteCount = 0;
 
@@ -24,34 +28,40 @@ for (var i = 0; i < imageNameList.length; i++) {
   new NewImage(imageNameList[i]);
 }
 
+// helper functions
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
-var recentRandomNumbers = [];
+
+var recentRandomIndexes = [];
 function getRandomIndex() {
   var randomIndex = getRandomNumber(0, allImages.length - 1);
 
-  while (recentRandomNumbers.includes(randomIndex)) {
+  while (recentRandomIndexes.includes(randomIndex)) {
     randomIndex = getRandomNumber(0, allImages.length - 1);
   }
 
-  if (recentRandomNumbers.length > 5) {
-    recentRandomNumbers.shift(); // may need to adjust number on this
+  if (recentRandomIndexes.length > 5) {
+    recentRandomIndexes.shift();
   }
-  recentRandomNumbers.push(randomIndex);
+  recentRandomIndexes.push(randomIndex);
   //console.log(randomIndex);
 
   return randomIndex;
 }
 
 
-// helper functions
-function getRandomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+
 
 function handleClick() {
   var chosenImage = event.target.title;
+  console.log('event.target.title is', event.target.title); // WORKING ON THIS
   for (var i = 0; i < allImages.length; i++) {
-    if (allImages[i] === chosenImage) {
+    console.log('I am looping i handleClick: ' + i);
+    console.log('I am about to compare these:', allImages[i], chosenImage);
+    if (allImages[i].name === chosenImage) {
+      console.log('I am going to give this one a vote: ' + chosenImage);
       allImages[i].voteCount++;
     }
   }
@@ -61,10 +71,11 @@ function handleClick() {
     render();
   } else {
     imageBoxEl.removeEventListener('click', handleClick);
+    renderVoteCount();
   }
 }
 
-// CREAT HELPER FUNCTION
+// CREATE HELPER FUNCTION
 function render() {
   var pictureIndex = getRandomIndex();
   allImages[pictureIndex].viewCount++;
@@ -85,9 +96,15 @@ function render() {
   imageThreeEl.title = allImages[pictureIndex].name;
 }
 
+function renderVoteCount() {
+  for (var i = 0; i < allImages.length; i++) {
+    var liEl = document.createElement('li');
+    liEl.textContent = `${allImages[i].voteCount} votes for ${allImages[i].name}`;
+    ulEl.appendChild(liEl);
+  }
+}
+
 imageBoxEl.addEventListener('click', handleClick);
-
-
 
 render();
 
